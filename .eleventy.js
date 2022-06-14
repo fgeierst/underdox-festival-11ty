fedd636911826ebce65450c0e4644811bde850b1
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2022-06-14 23:09:31 +0200

    basic 11ty config

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/grafik");
   // Return your Object options:
   return {
     dir: {
